b1788eb2813b989fc211a253781212e09ebcad28
qingkong3254 <qingkong32544@gmail.com>
2026-01-07 23:14:03 +0800

    Delete assets directory

--- assets/cursor-D4Y7uNdG.js
+++
@@ -1,3 +0,0 @@
-import{w as o}from"./core-C1TJwvW4.js";import"./index-lt7kYIz7.js";import"./index.es-DmCGUx5s.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
-  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
-</svg>`;export{l as cursorSvg};
